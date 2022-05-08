104416cf2c09d5e14461eedf46d46bd02c5fde64
goncalon <goncalosilvan@gmail.com>
2022-05-08 23:54:34 +0100

    2 tentativa

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,4 +1,53 @@
-module.exports = {
+const mysql = require('mysql2');
+const { Client } = require('ssh2');
+const sshClient = new Client();
+const dbServer = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+}
+const tunnelConfig = {
+    host: process.env.DB_SSH_HOST,
+    port: 22,
+    username: process.env.DB_SSH_USER,
+    password: process.env.DB_SSH_PASSWORD
+}
+const forwardConfig = {
+    srcHost: '127.0.0.1',
+    srcPort: 3306,
+    dstHost: dbServer.host,
+    dstPort: dbServer.port
+};
+const SSHConnection = new Promise((resolve, reject) => {
+    ssh.on('ready', () => {
+        ssh.forwardOut(
+        forwardConfig.srcHost,
+        forwardConfig.srcPort,
+        forwardConfig.dstHost,
+        forwardConfig.dstPort,
+        (err, stream) => {
+             if (err) reject(err);
+             const updatedDbServer = {
+                 ...dbServer,
+                 stream
+            };
+            const connection =  mysql.createConnection(updatedDbServer);
+           connection.connect((error) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(connection);
+            });
+        });
+    }).connect(tunnelConfig.sshConfig);
+});
+
+module.exports = SSHConnection;
+
+/* {
+    
     HOST: '165.22.123.116',
     USER: 'root',
     PASSWORD: '',
@@ -11,4 +60,4 @@ module.exports = {
         acquire: 30000,
         idle: 10000
     }
-}
+} */
