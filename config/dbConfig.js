84a061ee9aab5859ee0a8c6aa5b96a695b4408f6
goncalon <goncalosilvan@gmail.com>
2022-05-09 00:07:57 +0100

    6

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -6,7 +6,8 @@ const dbServer = {
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    dialect: 'mysql'
 }
 const tunnelConfig = {
     host: process.env.DB_SSH_HOST,
